e17b2d8945d0f18f367b13ea6f1309f7ef5a6cbd
SURENDAR-747 <vijaysurendar2450@gmail.com>
2026-03-30 20:42:57 +0530

    Update your_unit.v

--- template/src/your_unit.v
+++ template/src/your_unit.v
@@ -1,13 +1,13 @@
-module your_some_unit #(parameter WIDTH = 8) (
-    input clk,
-    input rst,
-    input [WIDTH-1:0] a,
-    input [WIDTH-1:0] b,
-    output reg [2*WIDTH-1:0] out
+module alu #(parameter WIDTH = 8) (
+    input [1:0] op_sel,      // 00:MAC, 01:SUB, 10:DIV
+    input [WIDTH-1:0] a, b, c, 
+    output [2*WIDTH-1:0] out
 );
+    wire [7:0] mac_res = (a * b) + c;
+    wire [7:0] sub_res = a - b; 
+    wire [7:0] div_res = (b != 0) ? (a / b) : 8'b0; 
 
-    always @(posedge clk or posedge rst) begin
-      // your implementation here
-    end
-
+    assign out = (op_sel == 2'b00) ? {8'b0, mac_res} :
+                 (op_sel == 2'b01) ? {8'b0, sub_res} :
+                 (op_sel == 2'b10) ? {8'b0, div_res} : 16'b0; 
 endmodule
